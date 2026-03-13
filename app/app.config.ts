affb1686b424c5a7c9c91fdf3135da64a16c9d13
ZMou <qiqi20020612@126.com>
2026-03-13 22:25:16 +0800

    Update slugRegex pattern in app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -7,7 +7,8 @@ export default defineAppConfig({
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
-  slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
+  // slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
+  slugRegex: /^[a-z0-9\/]+(?:-[a-z0-9\/]+)*$/i,
   reserveSlug: [
     'dashboard',
   ],
