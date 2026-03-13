d936a6a181151406fa4f870df45a96c574153f9b
ZMou <qiqi20020612@126.com>
2026-03-13 22:33:39 +0800

    fix: 只允许 re/ 作为前缀
    Update slugRegex pattern for URL validation

--- app/app.config.ts
+++ app/app.config.ts
@@ -8,7 +8,7 @@ export default defineAppConfig({
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   // slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
-  slugRegex: /^[a-z0-9\/]+(?:-[a-z0-9\/]+)*$/i,
+  slugRegex: /^(?:re\/)?[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
     'dashboard',
   ],
